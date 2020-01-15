0fed7325115ef271f007a4c73fdbd2bd31c30088
Matthew Long <mlong@magneton.io>
2020-01-15 20:58:26 +0000

    added sql update script

---
+++ updateurl.sql
@@ -0,0 +1,2 @@
+UPDATE wp_options SET option_value = 'https://sunocolp.thirdwavestage.com' WHERE option_name = 'siteurl';
+UPDATE wp_options SET option_value = 'https://sunocolp.thirdwavestage.com' WHERE option_name = 'home';
